6cf2dea2537bf5e9d6a565f03df435d656877407
蘑菇包子 <110318874@qq.com>
2018-06-25 14:13:40 +0800

    modify readme.me and add svg

--- README.md
+++ README.md
@@ -9,7 +9,26 @@ or
 ```
 npm install -g create-react-app
 ```
-进入项目根目录
+- 进入项目根目录
 ```
 cd react-app
 ```
+- 第一次请先执行
+```
+cnpm install
+```
+- 启动
+```
+npm run dev
+```
+- 部署
+```
+npm run build 
+```
+插件
+
+
+插件名称 | 作用 
+--- | --- 
+react-router-dom | react路由跳转
+redux | 组件间的相互通信
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,20 +1,67 @@
 import React,{Component} from 'React'
 import "./main.css"
+import cell from "../../images/svg/cellphone.svg"
+import eva from "../../images/svg/evaluate.svg"
+import email from "../../images/svg/email.svg"
+import workExp from "../../images/svg/workExp.svg"
+import stuExp from "../../images/svg/stuExp.svg"
+import skill from "../../images/svg/skill.svg"
+import perIcon from "../../images/svg/personMessage.svg"
+import githubImage from '../../images/forkme.png'
+
 export default class Main extends Component {
     render(){
         return (<div className={`page`}>
-             <p className={`title`}>blank page</p>
-            {/* <div>
-                 <ul className={`resumeUl`}>
-                     <li>工作经历</li>
-                 </ul>
-                 <ul className={`resumeUl`}>
-                     <li>2</li>
-                 </ul>
-                 <ul className={`resumeUl`}>
-                     <li>3</li>
-                 </ul>
-             </div>*/}
+            <div className={`triangleImage`}>
+                <a className={`forkMyGithub`} href={`https://github.com/LancelotSaki/react-app`}>
+                    <img src={githubImage}/>
+                </a>
+            </div>
+            <div className={`myPanel`}>
+                <div className={`myHeadPhoto`}></div>
+                <div className={`myPhoto`}></div>
+                <div className={`personMess`}>
+                    <ul className={`perUl`}>
+                        <li>
+                            <img src={cell} className={`cellImage`}/>
+                        </li>
+                        <li> 18888888888</li>
+                    </ul>
+                    <ul className={`perUl`}>
+                        <li>
+                            <img src={eva} className={`cellImage`}/>
+                        </li>
+                        <li> 18888888888</li>
+                    </ul>
+                    <ul className={`perUl`}>
+                        <li>
+                            <img src={email} className={`cellImage`}/>
+                        </li>
+                        <li>1103418874@qq.com</li>
+                    </ul>
+                    <ul className={`perUl`}>
+                        <li>
+                            <img src={workExp} className={`cellImage`}/>
+                        </li>
+                        <li> 18888888888</li>
+                    </ul>
+                    <ul className={`perUl`}>
+                        <li>
+                            <img src={stuExp} className={`cellImage`}/>
+                        </li>
+                        <li> 18888888888</li>
+                    </ul>
+                    <ul className={`perUl`}>
+                        <li>
+                            <img src={skill} className={`cellImage`}/>
+                        </li>
+                        <li> 18888888888</li>
+                    </ul>
+                </div>
+
+                <div className={`myMess`}></div>
+
+            </div>
         </div>)
     }
 }
