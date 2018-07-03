67947e4cdb52bd65c5fba44a510858cec02bb079
蘑菇包子 <110318874@qq.com>
2018-07-03 18:06:45 +0800

    添加skill进度条

--- README.md
+++ README.md
@@ -17,6 +17,77 @@ cd react-app
 ```
 cnpm install
 ```
+- 安装两个插件(antd启动准备需要)
+```
+cnpm install react-app-rewired --dev
+```
+和
+```
+cnpm install babel-plugin-import --dev
+```
+- 启动准备
+  由于项目使用官方推荐的create-react-app创建，因而我们没必要执行react-scripts eject把配置文件解包出来
+  - 启用css module(默认关闭) 
+  ```
+  node_modules/_react-scripts@xxxxx@react-scripts/config/
+  ```
+  找到
+  ```
+  webpack.config.dev.js  //开发
+  webpack.config.prod.js //生产
+  ```
+  搜索css-loader 在options里加入 
+  ```
+   modules: true 
+  ```
+  开发：
+  ```
+  require.resolve('style-loader'),
+                {
+                  loader: require.resolve('css-loader'),
+                  options: {
+                    importLoaders: 1,
+                      modules: true
+                  },
+                },
+  ```
+  生产：
+  ```
+  loader: require.resolve('css-loader'),
+                        options: {
+                          importLoaders: 1,
+                          minimize: true,
+                          modules: true
+                          sourceMap: shouldUseSourceMap,
+                        },
+  ```
+  - 兼容antd样式
+  找到
+  ```
+    webpack.config.dev.js  //开发
+    webpack.config.prod.js //生产
+  ```
+    中style-loader那片段在test: /\.css$/,后面加入排除判断
+  ```
+    test: /\.css$/,
+    exclude: /node_modules|material-design-lite\.scss/,
+  ```
+  新增一段跟上面差不多的样式匹配
+  ```
+  {//antd样式处理
+                  test:/\.css$/,
+                  include: /node_modules|material-design-lite\.scss/,
+                  use:[
+                      { loader: "style-loader",},
+                      {
+                          loader: "css-loader",
+                          options:{
+                              importLoaders:1
+                          }
+                      }
+                  ]
+              },
+   ```
 - 启动
 ```
 npm run dev
@@ -28,7 +99,9 @@ npm run build
 插件
 
 
-插件名称 | 作用 
---- | --- 
-react-router-dom | react路由跳转
-redux | 组件间的相互通信
+插件名称 | 作用 | 地址
+--- | --- | ---
+reactJs | 该项目核心 | [官方文档](https://reactjs.org/docs/hello-world.html)
+antd | 前端UI库 | [学习地址](https://ant.design/docs/react/introduce-cn)
+react-router-dom | react路由跳转 | [参考地址](http://reacttraining.cn/web/guides/quick-start)
+redux | 组件间的相互通信 | -
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,86 +1,179 @@
 import React, {Component} from 'React'
 import m from "./main.css"
+import { Tooltip } from 'antd';
+import { Timeline, Icon } from 'antd';
+import { Progress } from 'antd';
+let initInfoMap = new Map();
+initInfoMap.set("name", "Carney")
+    .set("phone","1888888888")
+    .set("email","111111111@gmail.com")
+    .set("university","GuangXi University")
+    .set("major", "计算机学院网络工程")
+    .set("address","广西南宁市西乡塘区大学路");
+
+function MyInformation(p) {
+    return (<div className={m.personMess}>
+        <div className={m.phone} data={p.phone}/>
+        <div className={m.email} data={p.email}/>
+        <div className={m.university} data={p.university}/>
+        <div className={m.major} data={p.major}/>
+        <div className={m.address} data={p.address}/>
+    </div>);
+}
+
+function MySkill(p) {
+    const label = (<div className={m.skillProgress}>
+        {p.skills.map((skill) =>
+                <Tooltip key={skill.key} placement={`right`} title={skill.title}>
+                    <div className={
+                        skill.name === 'java' ? m.myJava :
+                            skill.name === 'linux' ? m.myLinux :
+                                skill.name === 'oracle' ? m.myOracle :
+                                    skill.name === 'react' ? m.myReact :
+                                        skill.name === 'mysql' ? m.mySql :
+                                            skill.name === 'vue' ? m.myVue :
+                                                skill.name === 'javaScript' ? m.myJavaScript :
+                                                    skill.name === 'css' ? m.myCss :
+                                                        skill.name === 'jquery' ? m.myJquery : ``}>
+                        <Progress key={skill.key} percent={skill.value} size="small"
+                                  showInfo={false}
+                                  status = {skill.value >= 75 ?`success`:skill.value <= 50 ? `exception`:
+                                      skill.value < 75&& skill.value > 60?`active`:`normal`}
+                                  />
+                    </div>
+                </Tooltip>)}
+    </div>);
+
+    return label;
+}
+
+function MyWork() {
+    return (<div className={m.myWorkDetail}>
+        <div className={m.workExp}><div/></div>
+        <div className={m.workTimeLine}>
+            <Timeline>
+                <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">2014.01.01-2015.06.07</Timeline.Item>
+                <Timeline.Item>I</Timeline.Item>
+                <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} color="pink"/>}>2015.07.01-2016.12.31</Timeline.Item>
+                <Timeline.Item>am</Timeline.Item>
+                <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>2017.01.01-2018.12.31</Timeline.Item>
+                <Timeline.Item>02</Timeline.Item>
+            </Timeline>
+        </div>
+    </div>);
+}
+
+function MyProject() {
+    return (<div className={m.projectDetail}>
+        <div className={m.projectExp}><div/></div>
+        <div className={m.workTimeLine}>
+            <Timeline>
+                <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">2014.01.01-2015.06.07</Timeline.Item>
+                <Timeline.Item>I</Timeline.Item>
+                <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} color="pink"/>}>2015.07.01-2016.12.31</Timeline.Item>
+                <Timeline.Item>am</Timeline.Item>
+                <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>2017.01.01-2018.12.31</Timeline.Item>
+                <Timeline.Item>02</Timeline.Item>
+            </Timeline>
+        </div>
+    </div>);
+}
+
 export default class Main extends Component {
+    constructor(pro){
+       super(pro)
+        this.state = {
+                name : "",
+                phone: "",
+                email: "",
+                university: "",
+                major: "",
+                address: "",
+                currentState : "",
+                skill: []
+        };
+    }
+    // 只调用一次
+     componentDidMount(){
+         this.setState({
+         skill:[{key:1,name:"java",value:75,title:"This is Java!"},
+             {key:2,name:"linux",value:62,title:"This is Linux!"},
+             {key:3,name:"oracle",value:63,title:"This is Oracle!"},
+             {key:4,name:"mysql",value:65,title:"This is Mysql!"},
+             {key:5,name:"react",value:60,title:"This is React!"},
+             {key:6,name:"vue",value:68,title:"This is Vue!"},
+             {key:7,name:"javaScript",value:58,title:"This is JavaScript!"},
+             {key:8,name:"jquery",value:67,title:"This is Jquery!"},
+             {key:9,name:"css",value:50,title:"This is Css!"},]
+        });
+
+         //等待3s后执行打字
+         window.setTimeout(()=>{
+             this.executeAwait(initInfoMap);
+         },3000);
+     }
+
+   /* componentDidUpdate(props,state){
+          console.log(state)
+    }*/
+
+    /*打字效果*/
+    returnMyself(key, value) {
+        let x = 0;
+        return new Promise((res) => {
+            this.timer = setInterval(()=> {
+                let st = `{"${key}":"${value.substring(0, x++)}"}`;
+                this.setState(JSON.parse(st));
+                if (value.length - x < 0) {//设置执行一段时间后停止，要不然定时器会一直执行下去
+                    clearInterval(this.timer);
+                    res(key)
+                }
+            },100);
+        })
+    }
+
+
+    /*同步执行*/
+    async executeAwait(map) {
+        for (let [key, value] of map) {
+            //await等待该方法执行完才能继续下一个循环
+            let returnKey = await this.returnMyself(key, value);
+            if(returnKey === 'address') {
+                this.setState({currentState:"address"});
+            }
+            console.log("lastKey:"+returnKey)
+        }
+    }
+
     render() {
+
+        const info = {
+            phone: this.state.phone,
+            email: this.state.email,
+            university: this.state.university,
+            major: this.state.major,
+            address: this.state.address
+        }
+
         return (<div className={m.page}>
             {/*这是右上角的内容*/}
+            <div className={m.triangleCatImage}/>
             <a className={m.forkMyGithub} rel="nofollow noopener noreferrer"
                href="https://github.com/LancelotSaki/react-app"
                target="_blank"> </a>
-            <div className={m.triangleCatImage}/>
             {/*这是面板层*/}
             <div className={m.myPanel}>
                 {/*这是头像*/}
-                <div className={m.myHeadPhoto}/>
-                <div className={m.myPhoto}/>
-                <div className={m.showMyName}>Carney</div>
-
-                <div className={m.personMess}>
-                    <div className={m.perCellPhone}>1888888887</div>
-                    <div className={m.perEva}>评价</div>
-                    <div className={m.perEmail}>1103418874@qq.com</div>
-                    <div className={m.perStu}>教育经历</div>
-                </div>
-
-
-                <div className={m.skillProgress}>
-                    <progress value={`0.78`} className={m.myJavaProgress}>o(︶︿︶)o就不能换个好点的浏览器?
-                    </progress>
-                    <progress value={`0.55`} className={m.myLinuxProgress}/>
-                    <progress value={`0.60`} className={m.myOracleProgress}/>
-                    <progress value={`0.58`} className={m.mySqlProgress}/>
-                    <progress value={`0.6`} className={m.myReactProgress}/>
-                    <progress value={`0.65`} className={m.myVueProgress}/>
-                    <progress value={`0.5`} className={m.myJavaScriptProgress}/>
-                    <progress value={`0.54`} className={m.myJqueryProgress}/>
-                    <progress value={`0.4`} className={m.myCssProgress}/>
-                    <progress value={`0.6`} className={m.myExtJsProgress}/>
-                </div>
-
+                <div className={m.headPhotoCircle}/>
+                <div className={m.showMyName} data={this.state.name}/>
+                 {/*个人信息*/}
+                <MyInformation {...info}/>
+                {/*我的技能*/}
+                <MySkill skills={this.state.skill}/>
                 {/*这是右下角个人详细信息层*/}
-                <div className={m.myMessageDetail}>
-                    <div className={m.workLeftTopLine}/>
-                    <div className={m.workExp}>
-                        <div/>
-                    </div>
-                    <div className={m.workTable}>
-                        <ul>
-                            <li>2014.01.01-2015.06.07</li>
-                            <li>2</li>
-                        </ul>
-                        <ul>
-                            <li>2015.07.01-2016.12.31</li>
-                            <li>2</li>
-                        </ul>
-                        <ul>
-                            <li>2017.01.01-2018.12.31</li>
-                            <li>2</li>
-                        </ul>
-                    </div>
-                    <div className={m.workLeftBottomLine}/>
-                </div>
-                {/*这是个人项目经历*/}
-                <div className={m.projectDetail}>
-                    <div className={m.projectLeftTopLine}></div>
-                    <div className={m.projectExp}>
-                        <div/>
-                    </div>
-                    <div className={m.projectLeftBottomLine}></div>
-                    <div className={m.expTable}>
-                        <ul>
-                            <li>1</li>
-                            <li>2</li>
-                        </ul>
-                        <ul>
-                            <li>1</li>
-                            <li>2</li>
-                        </ul>
-                        <ul>
-                            <li>1</li>
-                            <li>2</li>
-                        </ul>
-                    </div>
-                </div>
+                <MyWork/>
+                {/*这是个人项目*/}
+                <MyProject/>
             </div>
         </div>)
     }
